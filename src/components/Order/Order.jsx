9c97441c3807a2b1051976b97f43d866327e00d0
ElenaS35 <helena0518savitskaya@gmail.com>
2022-12-30 11:41:20 +0200

    new

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -9,11 +9,13 @@ class Order extends Component {
                 <div className='item-image'>
                     <img src={"./img/" + this.props.item.img} alt='img' />
                 </div>
+                <div className='item-text'>
+                    <h2>{this.props.item.title}</h2>
+                    <b>{this.props.item.priceTotal} $</b>    
+                </div>
                 <div className='item-count'>
 				    <Count count={this.props.item.count} increase={this.props.increase} decrease={this.props.decrease} changeValue={this.props.changeValue} id={this.props.item.id} />
-			    </div>
-                <h2>{this.props.item.title}</h2>
-                <b>{this.props.item.priceTotal} $</b>
+			    </div>    
                 <FaTrash className='delete-icon' onClick={() => this.props.onDelete(this.props.item.id)}/>
             </div>)
     }
